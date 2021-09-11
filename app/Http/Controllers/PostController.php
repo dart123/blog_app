42165a1efa2e9013795ba7b5ca49ea9c36e9d365
dart123 <aleksandr.zemyan@gmail.com>
2021-09-12 00:22:32 +0300

    Created seeders with factories, wrote some basic retrieval logic for posts, wrote services

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PostService;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    private $postService;
+    public function __construct(PostService $service)
+    {
+        $this->postService = $service;
+    }
+
     public function index() {
+        $posts = $this->postService->getLastPosts(6);
+        $active_menu_item = '';
         return view('welcome');
     }
     public function getAll() {
-
+        $posts = $this->postService->getAllPosts(10);
     }
 
     public function getSingle($slug) {
--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -2,7 +2,25 @@
 
 namespace App\Services;
 
+use App\Models\Post;
+
 class PostService
 {
+    public function getLastPosts($post_num = 6) {
+        return Post::select('*')
+            ->orderBy('created_at', 'DESC')
+            ->limit($post_num)
+            ->get();
 
+    }
+
+    public function getAllPosts($per_page = 10) {
+        return Post::select('*')
+            ->orderBy('created_at', 'DESC')
+            ->paginate($per_page);
+    }
+
+    public function getPostBySlug($slug) {
+
+    }
 }
